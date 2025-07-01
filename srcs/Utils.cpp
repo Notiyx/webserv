96ef60e1ff51cfd505ab303274d6d7d1c15a6c67
Nokiki <nokiyxdev@gmail.com>
2025-07-01 06:50:45 +0200

    opti na;espace

--- srcs/Utils.cpp
+++ srcs/Utils.cpp
@@ -6,20 +6,16 @@
 /*   By: tlonghin <tlonghin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/01 06:40:37 by tlonghin          #+#    #+#             */
-/*   Updated: 2025/07/01 06:49:18 by tlonghin         ###   ########.fr       */
+/*   Updated: 2025/07/01 06:50:33 by tlonghin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <Utils.hpp>
 
-
-
-namespace utils {
-    bool    checkEndStr(const std::string str, const std::string suffix) {
-        if (str.length() < suffix.length())
-            return (false);
-        if (str.compare(str.length() - suffix.length(), suffix.length(), suffix) != 0)
-            return (false);
-        return (true);
-    }
+bool    utils::checkEndStr(const std::string str, const std::string suffix) {
+    if (str.length() < suffix.length())
+        return (false);
+    if (str.compare(str.length() - suffix.length(), suffix.length(), suffix) != 0)
+        return (false);
+    return (true);
 }
